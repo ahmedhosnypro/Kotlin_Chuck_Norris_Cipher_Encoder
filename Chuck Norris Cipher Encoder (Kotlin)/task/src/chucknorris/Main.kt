15ae611eab7c94a5c8a0409110cf6e25af5d3133
Ahmed Hosny <ahhosnyas@gmail.com>
2023-07-13 22:36:57 +0300

    Stage 3/5: Chuck Norris encrypts only with zeros

--- Chuck Norris Cipher Encoder (Kotlin)/task/src/chucknorris/Main.kt
+++ Chuck Norris Cipher Encoder (Kotlin)/task/src/chucknorris/Main.kt
@@ -1,13 +1,31 @@
 package chucknorris
 
 fun main() {
+    val stringBuilder = StringBuilder()
+    var seqChar = "-1"
     println("Input string:\n")
-    readln()
-        .apply {
-            println("The result:")
-        }
-        .toCharArray()
-        .forEach {
-            println("$it = ${it.code.toString(2).padStart(7, '0')}")
-        }
+    readln().apply {
+        println("The result:")
+    }.toCharArray().forEach { ch ->
+        ch.code.toString(2).padStart(7, '0')
+            .toCharArray().onEach {
+                if (it == '0') {
+                    if (seqChar == "0") {
+                        stringBuilder.append("0")
+                    } else {
+                        stringBuilder.append(" 00 0")
+                        seqChar = "0"
+                    }
+                } else {
+                    if (seqChar == "1") {
+                        stringBuilder.append("0")
+                    } else {
+                        stringBuilder.append(" 0 0")
+                        seqChar = "1"
+                    }
+                }
+            }
+    }
+
+    print(stringBuilder.toString().trim())
 }

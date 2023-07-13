9b2c13bc3a820a3a8e822664e2f7f2bf6a66d623
Ahmed Hosny <ahhosnyas@gmail.com>
2023-07-13 23:18:58 +0300

    Stage 4/5: Try to understand it

--- Chuck Norris Cipher Encoder (Kotlin)/task/src/chucknorris/Main.kt
+++ Chuck Norris Cipher Encoder (Kotlin)/task/src/chucknorris/Main.kt
@@ -1,27 +1,41 @@
 package chucknorris
 
 fun main() {
+
     println("Input string:\n")
-
-    val encryptedMessage = StringBuilder()
-    var seqChar = ' '
-
     readln().apply {
         println("The result:")
-    }.toCharArray().forEach { ch ->
-        ch.code
-            .toString(2)
-            .padStart(7, '0')
-            .toCharArray()
-            .onEach {
+    }.let { decode(it) }.let(::println)
+}
+
+
+fun encode(message: String): String {
+    val encodedMessage = StringBuilder()
+    var seqChar = ' '
+    message.toCharArray().forEach { ch ->
+        ch.code.toString(2).padStart(7, '0')
+            .toCharArray().onEach {
                 if (it == seqChar) {
-                    encryptedMessage.append("0")
+                    encodedMessage.append("0")
                 } else {
                     seqChar = it
-                    encryptedMessage.append(" ${if (it == '1') "0 " else "00 "}0")
+                    encodedMessage.append(" ${if (it == '1') "0 " else "00 "}0")
                 }
             }
     }
+    return encodedMessage.toString().trim()
+}
 
-    print(encryptedMessage.toString().trim())
+fun decode(encodeMessage: String): String {
+    val decodedMessage = StringBuilder()
+    var seqChar = ' '
+    encodeMessage.split(" ").forEachIndexed() { index, s ->
+        if (index % 2 == 0) {
+            seqChar = if (s == "00") '0' else '1'
+        } else {
+            decodedMessage.append(seqChar.toString().repeat(s.length))
+        }
+    }
+
+    return decodedMessage.chunked(7).map { it.toInt(2).toChar() }.joinToString("")
 }

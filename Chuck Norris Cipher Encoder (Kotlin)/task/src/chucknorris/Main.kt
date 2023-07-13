92366ea327df1f8b42b754f40ca86782821b89d1
Ahmed Hosny <ahhosnyas@gmail.com>
2023-07-13 22:40:16 +0300

    optimize stage 3

--- Chuck Norris Cipher Encoder (Kotlin)/task/src/chucknorris/Main.kt
+++ Chuck Norris Cipher Encoder (Kotlin)/task/src/chucknorris/Main.kt
@@ -2,27 +2,18 @@ package chucknorris
 
 fun main() {
     val stringBuilder = StringBuilder()
-    var seqChar = "-1"
+    var seqChar = ' '
     println("Input string:\n")
     readln().apply {
         println("The result:")
     }.toCharArray().forEach { ch ->
         ch.code.toString(2).padStart(7, '0')
             .toCharArray().onEach {
-                if (it == '0') {
-                    if (seqChar == "0") {
-                        stringBuilder.append("0")
-                    } else {
-                        stringBuilder.append(" 00 0")
-                        seqChar = "0"
-                    }
+                if (it == seqChar) {
+                    stringBuilder.append("0")
                 } else {
-                    if (seqChar == "1") {
-                        stringBuilder.append("0")
-                    } else {
-                        stringBuilder.append(" 0 0")
-                        seqChar = "1"
-                    }
+                    seqChar = it
+                    stringBuilder.append(" ${if (it == '1') "0 " else "00 "}0")
                 }
             }
     }
